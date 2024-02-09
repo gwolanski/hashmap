3eb60d70a13c729ec2837e70210d03c7ebb94560
gwolanski <gwol@udel.edu>
2024-02-09 08:34:32 -0700

    hash function returns correct hash code

--- hashmap.js
+++ hashmap.js
@@ -8,7 +8,7 @@ function hashMap() {
                
             const primeNumber = 31;
             for (let i = 0; i < key.length; i++) {
-              hashCode = [primeNumber * hashCode + key.charCodeAt(i)] % arrayLength;
+                hashCode = [primeNumber * hashCode + key.charCodeAt(i)] % arrayLength;
             }
          
             return hashCode;
--- hashmap.test.js
+++ hashmap.test.js
@@ -4,4 +4,9 @@ test('If key is not string, throw error', () => {
     const newHashMap = hashMap();
     const badHash = () => newHashMap.hash(502, 10);
     expect(badHash).toThrow(Error);
-})
+}),
+
+test('Key of the word cat should return hashCode of 2', () => {
+    const newHashMap = hashMap();
+    expect(newHashMap.hash('cat', 5)).toBe(2);
+} )
