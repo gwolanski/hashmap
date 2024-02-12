438f30b04c07d684a9fdd6545fd79f194c6fba3a
gwolanski <gwol@udel.edu>
2024-02-12 07:13:42 -0700

    set functionality

--- hashmap.js
+++ hashmap.js
@@ -1,19 +1,87 @@
-function hashMap() {
-    return {
-        hash(key, arrayLength) {
-            if (typeof key !== 'string') {
-                throw new Error('Key must be a string')
-            }
-            let hashCode = 0;
-               
-            const primeNumber = 31;
-            for (let i = 0; i < key.length; i++) {
-                hashCode = [primeNumber * hashCode + key.charCodeAt(i)] % arrayLength;
-            }
-         
-            return hashCode;
-          }
+class Node {
+    constructor(key, value) {
+        this.key = key;
+        this.value = value;
+        this.next = null
     }
 }
 
-module.exports = hashMap;
+class HashMap {
+    constructor() {
+        this.bucketCapacity = 16,
+        this.loadFactor = 0.75,
+        this.buckets= []
+    }
+
+    hash(key) {
+        if (typeof key !== 'string') {
+            throw new Error('Key must be a string')
+        }
+        let hashCode = 0;   
+        const primeNumber = 31;
+
+        for (let i = 0; i < key.length; i++) {
+            hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.bucketCapacity;
+        }
+     
+        return hashCode;
+      }
+    
+    set(key, value) {
+        //add logic to check load factor and grow if necessary 
+        
+        const index = this.hash(key);
+        console.log(`key ${key}, index ${index}`)
+        const newNode = new Node(key, value);
+
+        if (index < 0 || index >= this.bucketCapacity) {
+            throw new Error('Trying to access index out of bounds');
+        } else {
+            if (!this.buckets[index]) {
+                this.buckets[index] = newNode;
+            } else {
+                let currentNode = this.buckets[index];
+                while (currentNode) {
+                    if (currentNode.key === key) {
+                        currentNode.value = value;
+                        return;
+                    }
+                    if (!currentNode.next) {
+                        break;
+                    }
+                    currentNode = currentNode.next;
+                }
+                currentNode.next = newNode;
+                console.log('currentNode.next: ' + currentNode.next)
+            } 
+        }
+    }
+}
+
+module.exports = {
+    HashMap,
+    Node
+}
+
+//carlos 
+//C:  (31 * 0 + 67) % 16 = 3
+//a: (31* 3 + 97) % 16 = 14
+//r: (31 * 14 + 114) % 16 = 4
+//l: (31 * 4 + 108) % 16 = 8
+//o: (31 * 8 + 111) % 16 = 7
+//s: (31 * 7 + 115) % 16 = 12
+
+//banana
+//b: (31 * 0 + 98) % 16 = 2
+//a: (31 * 2 + 97) % 16 = 15
+//n: (31 * 5 + 110) % 16 = 9
+//a: (31 * 9 + 97) % 16 = 8
+//n: (31 * 8 + 110) % 16 = 6
+//a: (31 * 6 + 97) % 16 = 11
+
+//open 
+
+//o: (31 * 0 + 111) % 16 = 15
+//p: 
+//e: 
+//n: 
--- hashmap.test.js
+++ hashmap.test.js
@@ -1,12 +1,44 @@
-const hashMap = require('./hashmap')
+const HashMap = require('./hashmap').HashMap;
+const Node = require('./hashmap').Node;
 
 test('If key is not string, throw error', () => {
-    const newHashMap = hashMap();
+    const newHashMap = new HashMap();
     const badHash = () => newHashMap.hash(502, 10);
     expect(badHash).toThrow(Error);
 }),
 
-test('Key of the word cat should return hashCode of 2', () => {
-    const newHashMap = hashMap();
-    expect(newHashMap.hash('cat', 5)).toBe(2);
-} )
+test('Key of the word cat should return hashCode of 6 when arrayLength 16', () => {
+    const newHashMap = new HashMap();
+    expect(newHashMap.hash('cat')).toBe(6);
+}),
+
+test('HashMap set() function will set the key value pair at index from hash() function', () => {
+    const newHashMap = new HashMap();
+    newHashMap.set('Carlos', 'initial value');
+    let index = newHashMap.hash('Carlos');
+
+    expect(newHashMap.buckets[index].key).toBe('Carlos');
+    expect(newHashMap.buckets[index].value).toBe('initial value');
+}),
+
+test('HashMap set() function will update the value of a key if setting an existing key', () => {
+    const newHashMap = new HashMap();
+    newHashMap.set('Carlos', 'initial value');
+    newHashMap.set('Carlos', 'updated value');
+    let index = newHashMap.hash('Carlos');
+
+    expect(newHashMap.buckets[index].key).toBe('Carlos');
+    expect(newHashMap.buckets[index].value).toBe('updated value');
+}),
+
+test('HashMap set() function will create linked list if more than 1 key value pair at a certain index', () => {
+    const newHashMap = new HashMap();
+    newHashMap.set('AaBB', 'initial value');
+    newHashMap.set('BBAa', 'initial value');
+    newHashMap.set('AaAa', 'initial value');
+    let index = newHashMap.hash('AaBB');
+
+    expect(newHashMap.buckets[index].key).toBe('AaBB');
+    expect(newHashMap.buckets[index].next.key).toBe('BBAa');
+    expect(newHashMap.buckets[index].next.next.key).toBe('AaAa');
+})
